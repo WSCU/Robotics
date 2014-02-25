06b1334e5570d6ba42d17b2deae6d18ea1c9b0b9
Jiig <luktut@gmail.com>
2014-02-25 12:05:35 -0700

    Added the start of MapCreator and Nodes

---
+++ MapMaker.py
@@ -0,0 +1,41 @@
+import sys
+
+global d = {}
+maplength = 5
+world = [[Node() for x in xrange(maplength)] for x in xrange(maplength)] 
+unvisited = [()]
+
+def createMap():
+    #Setup the first Node
+    makeNode(maplength/2,maplength/2)
+    while (unvisited):
+        current = unvisited.pop()
+        
+
+def makeNode(i,j):
+    #Use the neato lasers to find the corner type
+    cornertype = """get the corner type"""
+    if cornertype is cross:
+        world[i][j].count = 4
+        world[i+1][j] = Node(1)
+        world[i-1][j] = Node(1)
+        world[i][j+1] = Node(1)
+        world[i][j-1] = Node(1)
+    if cornertype is l:
+        world[i][j].count = 2
+    if cornertype is t:
+        world[i][j].count = 3
+    if cornertype is deadend:
+        world[i][j].count = 1
+        
+def expandMap():
+    
+def main():
+    if (len(sys.args) > 1):
+        d["Initial Facing"] = sys.args[1]    
+    else 
+        d["Initial Facing"] = North
+
+    createMap()
+if __name__ == "__main__":
+    main()
---
+++ Node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self):
+        self.count = 0
+        self.visited = None
+    def __init(self, count):
+        self.count = count
+        self.visited = False
+
