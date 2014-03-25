10aadd10d3b9f0429059b6a4708f6cfd2b965204
pitafubar <lbggallagher@gmail.com>
2014-03-25 12:29:16 -0600

    Update MapMaker.py

--- MapMaker.py
+++ MapMaker.py
@@ -9,6 +9,7 @@ def createMap():
     #Setup the first Node
     makeNode(maplength/2,maplength/2)
     while (unvisited):
+        previous = current
         current = unvisited.pop()
         
 
@@ -17,17 +18,39 @@ def makeNode(i,j):
     cornertype = """get the corner type"""
     if cornertype is cross:
         world[i][j].count = 4
-        world[i+1][j] = Node(1)
-        world[i-1][j] = Node(1)
-        world[i][j+1] = Node(1)
-        world[i][j-1] = Node(1)
-    if cornertype is l:
+        if world[i+1][j]!=previous
+            world[i+1][j] = Node(1)
+        if world[i-1][j]!=previous
+            world[i-1][j] = Node(1)
+        if world[i][j+1]!=previous
+            world[i][j+1] = Node(1)
+        if world[i][j-1]!=previous
+            world[i][j-1] = Node(1)
+    if cornertype is l1:
         world[i][j].count = 2
+        if world[i][j+1] == previous
+            world[i][j+1]= Node(1)
+        if world[i][j-1] == previous
+            world[i][j-1]= Node(1)
+        world[i-1][j] = Node(1)
+    if cornertype is l2:
+        world[i][j].count = 2
+        if world[i][j+1] == previous
+            world[i][j+1]= Node(1)
+        if world[i][j-1] == previous
+            world[i][j-1]= Node(1)
+        world[i+1][j] = Node(1)
     if cornertype is t:
         world[i][j].count = 3
+        if world[i][j+1] == previous
+            world[i][j+1]= Node(1)
+        if world[i][j-1] == previous
+            world[i][j-1]= Node(1)
+        world[i-1][j] = Node(1)
+        wordl[i+1][j] = Node(1)
     if cornertype is deadend:
         world[i][j].count = 1
-        
+        world[i][j] = Node(1)
 def expandMap():
     
 def main():
