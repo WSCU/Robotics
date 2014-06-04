e2bc43e72e3d2ca2eb203f02e92574b9e7c2447c
pitafubar <lbggallagher@gmail.com>
2014-06-04 10:52:05 -0700

    Update hover.py
    Added.... nothing

--- hover.py
+++ hover.py
@@ -5,7 +5,26 @@ from std_msgs.msg import String
 from cflib.crazyflie import Crazyflie
 
 class Data: pass
-D = Data()
+    D = Data()
 
 def main():
-  
+    rospy.Subscriber("cf_logdata", String, callback)	
+
+def hoverCall(data):
+    global D
+    m = data.data
+    print ("Received message: " + m)
+    pitch,roll,thrust,gyrox,gyroy,gyroz,accx,accy,accz = m.split(" ")
+    """
+    if gyrox > 1 or gyrox < -1:
+        roll = -gyrox * 0.2
+    if gyroy > 1 or gyroy < -1:
+        pitch = -gyroy * 0.2
+
+
+
+
+
+    D.dataPub.publish(String())
+    """
+
