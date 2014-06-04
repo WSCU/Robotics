acfe33c3c4c5114b594a3e8b10d781730591f671
pitafubar <lbggallagher@gmail.com>
2014-06-04 15:58:02 -0700

    Update hover.py

--- hover.py
+++ hover.py
@@ -9,30 +9,47 @@ class Data: pass
     D = Data()
 
 def main():
-    rospy.Subscriber("cf_accData", AccelData, hoverCall)	
+    rospy.Subscriber("cf_accData", AccelData, accCall)	
     rospy.Subscriber("cf_stabData", StabData, hoverCall)	
     rospy.Subscriber("cf_gyroData", GyroData, hoverCall)	
     rospy.Subscriber("cf_motorData", MotorData, hoverCall)
-def hoverCall(data):
-    global D
-    m = data.data
-    print ("Received message: " + m)
-    pitch,roll,thrust,gyrox,gyroy,gyroz,accx,accy,accz = m.split(" ")
     
+    
+def accelCall(data):
+    global D
+    x = data.x
+    y = data.y
+    z = data.z
+    print ("Received message: " + x +","+ y +","+ z)
+    if accz<-1:
+        thrust= -accz*.2
+        D.dataPub.publish(String(t thrust))
+
+
+
+def stabCall(data):
+    global D
+    roll = data.roll
+    pitch = data.pitch
+    yaw = data.yaw
+    print("Received message:" + roll +","+ pitch + "," + yaw)
+    D.dataPub.publish(String(r roll))
+    D.dataPub.publish(String(p pitch))
+    D.dataPub.publish(String(y yaw))
+    
+def gyro(data):
+    global D
+    x = data.x
+    y = data.y
+    z = data.z
+    print ("Received message: " + x +","+ y +","+ z)
     if gyrox > 1 or gyrox < -1:
         roll = -gyrox * 0.2
     if gyroy > 1 or gyroy < -1:
         pitch = -gyroy * 0.2
     if gyroz > 1 or gyroz < -1:
         yaw = -gyroz * 0.2
-    if accz<-1:
-        thrust= -accz*.2
-        
-
-
-
-
     D.dataPub.publish(String(r roll))
     D.dataPub.publish(String(p pitch))
     D.dataPub.publish(String(y yaw))
-    D.dataPub.publish(String(t thrust))
+
