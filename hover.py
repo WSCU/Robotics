3e495227f1d4ab287e0e17c694969394a5098e9b
pitafubar <lbggallagher@gmail.com>
2014-06-05 10:14:53 -0700

    Update hover.py

--- hover.py
+++ hover.py
@@ -37,7 +37,7 @@ def stabCall(data):
     D.roll = data.roll
     D.pitch = data.pitch
     D.yaw = data.yaw
-    print("Stab: (roll, pitch, yaw):" + str(D.roll) +","+ str(D.pitch) + "," + str(D.yaw))
+    #print("Stab: (roll, pitch, yaw):" + str(D.roll) +","+ str(D.pitch) + "," + str(D.yaw))
     """
     D.dataPub.publish(String("r " + str(D.roll)))
     D.dataPub.publish(String("p " + str(D.pitch)))
@@ -49,7 +49,7 @@ def gyroCall(data):
     x = data.x
     y = data.y
     z = data.z
-    print ("Gyro (x, y, z): " + str(x) +","+ str(y) +","+ str(z))
+    #print ("Gyro (x, y, z): " + str(x) +","+ str(y) +","+ str(z))
     
     if x > 1 or x < -1:
         D.roll = -x * 0.2
@@ -69,7 +69,7 @@ def motorCall(data):
     m2 = data.m2
     m3 = data.m3
     m4 = data.m4
-    print("Motors (1, 2, 3, 4): "+str(m1)+","+str(m2)+","+str(m3)+","+str(m4))
+    #print("Motors (1, 2, 3, 4): "+str(m1)+","+str(m2)+","+str(m3)+","+str(m4))
     
 if __name__ == "__main__":
     main()    
